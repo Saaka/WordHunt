36181efe20c8df6ad7f30a60d949a761cf563dfd
Michał Sakwa <saka1988@gmail.com>
2017-04-01 23:01:25 +0200

    Changed imports.

--- WordHunt.Web/ClientApp/app/main/login/login.module.ts
+++ WordHunt.Web/ClientApp/app/main/login/login.module.ts
@@ -1,6 +1,4 @@
 ﻿import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule, routableComponents } from './login.routing.module';
 import { MainSharedModule } from '../shared/main-shared.module';
@@ -8,13 +6,7 @@ import { MainSharedModule } from '../shared/main-shared.module';
 @NgModule({
     imports: [
         LoginRoutingModule,
-        MainSharedModule,
-        FormsModule,
-        CommonModule
-    ],
-    exports: [
-        FormsModule,
-        CommonModule
+        MainSharedModule
     ],
     declarations: [
         routableComponents
--- WordHunt.Web/ClientApp/app/main/shared/main-shared.module.ts
+++ WordHunt.Web/ClientApp/app/main/shared/main-shared.module.ts
@@ -1,15 +1,21 @@
 ﻿import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 import { MainBackButtonComponent } from './main-back-button/main-back-button.component';
 
 @NgModule({
     imports: [
-        RouterModule
+        RouterModule,
+        FormsModule,
+        CommonModule
     ],
     exports: [
-        MainBackButtonComponent
+        MainBackButtonComponent,
+        FormsModule,
+        CommonModule
     ],
     declarations: [
         MainBackButtonComponent
