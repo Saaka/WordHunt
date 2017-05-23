ab777c520a3ddc56c6d39976046b269636e942fb
Michał Sakwa <saka1988@gmail.com>
2017-05-23 21:20:23 +0200

    Created login page.

--- WordHunt.Web/ClientApp/app/main/main.component.ts
+++ WordHunt.Web/ClientApp/app/main/main.component.ts
@@ -1,9 +1,10 @@
-﻿import { Component, OnInit, } from '@angular/core';
+﻿import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'main',
     templateUrl: './main.component.html',
+    encapsulation: ViewEncapsulation.None,
     styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
